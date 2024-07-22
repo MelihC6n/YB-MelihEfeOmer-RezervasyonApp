184f001e3be4fd6ec4049dcca2bb63b10461a6f8
Efe Can Demir <efecandemir7887@gmail.com>
2024-07-22 15:08:37 +0300

    Service'ler oluşturuldu.

---
+++ YB-MelihEfeOmer-RezervasyonApp.Business/Services/BookingService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YB_MelihEfeOmer_RezervasyonApp.Business.Abstractions;
+using YB_MelihEfeOmer_RezervasyonApp.DataAccess.Repositories;
+using YB_MelihEfeOmer_RezervasyonApp.Entity.Models;
+
+namespace YB_MelihEfeOmer_RezervasyonApp.Business.Services
+{
+    public class BookingService : IService<Booking>
+    {
+        private readonly BookingRepository _bookingRepository;
+        public BookingService(BookingRepository bRep)
+        {
+            _bookingRepository = bRep;
+        }
+        public void Add(Booking entity)
+        {
+            _bookingRepository.Add(entity);
+        }
+
+        public void Delete(Guid id)
+        {
+            _bookingRepository.Delete(id);
+        }
+
+        public IEnumerable<Booking>? GetAll()
+        {
+            return _bookingRepository.GetAll();
+        }
+
+        public Booking? GetById(Guid id)
+        {
+            return _bookingRepository.GetById(id);  
+        }
+
+        public void Update(Booking entity)
+        {
+            _bookingRepository.Update(entity);
+        }
+    }
+}
