929487527f0a84072011c9919d5208336ca19e94
Ömer Tanır <tanir.omer98@gmail.com>
2024-07-22 14:50:49 +0300

    In Data Access Layer IRepository added to Abstractions folder. Generic repositories added to Repositories folder. Henüz dbcontext olmadığı için bazı kodlar yorum satırı olarak bırakıldı.

---
+++ YB-MelihEfeOmer-RezervasyonApp.DataAccess/Repositories/GuestRepository.cs
@@ -0,0 +1,12 @@
+﻿using YB_MelihEfeOmer_RezervasyonApp.Entity.Models;
+
+namespace YB_MelihEfeOmer_RezervasyonApp.DataAccess.Repositories
+{
+    public class GuestRepository : GenericRepository<Guest>
+    {
+        //public GuestRepository(ApplicationDbContext context) : base(context)
+        //{
+            
+        //}
+    }
+}
---
+++ YB-MelihEfeOmer-RezervasyonApp.DataAccess/Repositories/StaffRepository.cs
@@ -0,0 +1,12 @@
+﻿using YB_MelihEfeOmer_RezervasyonApp.Entity.Models;
+
+namespace YB_MelihEfeOmer_RezervasyonApp.DataAccess.Repositories
+{
+    public class StaffRepository : GenericRepository<Staff>
+    {
+        //public StaffRepository(ApplicationDbContext context) : base(context)
+        //{
+            
+        //}
+    }
+}
