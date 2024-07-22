6e30e186aeff74a015d1a65b404ed0e4efb149fe
Melih Can Akgüneş <melihcanakgunes@gmail.com>
2024-07-22 10:41:51 +0300

    Add project files.

---
+++ YB-MelihEfeOmer-RezervasyonApp/Form1.cs
@@ -0,0 +1,10 @@
+namespace YB_MelihEfeOmer_RezervasyonApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
