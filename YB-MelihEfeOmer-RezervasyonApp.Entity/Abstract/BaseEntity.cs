9ee8deee25e414b18c70a5d855c59f9bf434890c
Melih Can Akgüneş <melihcanakgunes@gmail.com>
2024-07-22 11:35:54 +0300

    EntitiesCreated

---
+++ YB-MelihEfeOmer-RezervasyonApp.Entity/Abstract/BaseEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YB_MelihEfeOmer_RezervasyonApp.Entity.Abstract
+{
+    public class BaseEntity
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public DateTime CreatedTime { get; set; }
+        public DateTime UpdatedTime { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
---
+++ YB-MelihEfeOmer-RezervasyonApp.Entity/Models/Staff.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YB_MelihEfeOmer_RezervasyonApp.Entity.Abstract;
+
+namespace YB_MelihEfeOmer_RezervasyonApp.Entity.Models
+{
+    public class Staff : BaseEntity
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Position { get; set; }
+    }
+}
