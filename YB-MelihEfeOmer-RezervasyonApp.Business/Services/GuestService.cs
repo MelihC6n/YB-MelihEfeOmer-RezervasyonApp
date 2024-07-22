8f5ef718bdc618b0e3e203d1c6d1c2fd44c1859d
Melih Can Akgüneş <melihcanakgunes@gmail.com>
2024-07-22 16:12:36 +0300

    Reapply "Service'ler oluşturuldu."
    This reverts commit c0ebe1a6cbefb78cdc7dbe62567c4e8997330fa2.

---
+++ YB-MelihEfeOmer-RezervasyonApp.Business/Services/GuestService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YB_MelihEfeOmer_RezervasyonApp.Business.Abstractions;
+using YB_MelihEfeOmer_RezervasyonApp.DataAccess.Repositories;
+using YB_MelihEfeOmer_RezervasyonApp.Entity.Models;
+
+namespace YB_MelihEfeOmer_RezervasyonApp.Business.Services
+{
+    public class GuestService : IService<Guest>
+    {
+        private readonly GuestRepository _guestRepository;
+        public GuestService(GuestRepository gRep)
+        {
+            _guestRepository = gRep;
+        }
+        public void Add(Guest entity)
+        {
+            _guestRepository.Add(entity);
+        }
+
+        public void Delete(Guid id)
+        {
+            _guestRepository.Delete(id);
+        }
+
+        public IEnumerable<Guest>? GetAll()
+        {
+            return _guestRepository.GetAll();
+        }
+
+        public Guest? GetById(Guid id)
+        {
+            return _guestRepository.GetById(id);
+        }
+
+        public void Update(Guest entity)
+        {
+            _guestRepository.Update(entity);
+        }
+    }
+}
---
+++ YB-MelihEfeOmer-RezervasyonApp.Business/Services/RoomTypeService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YB_MelihEfeOmer_RezervasyonApp.Business.Abstractions;
+using YB_MelihEfeOmer_RezervasyonApp.DataAccess.Repositories;
+using YB_MelihEfeOmer_RezervasyonApp.Entity.Models;
+
+namespace YB_MelihEfeOmer_RezervasyonApp.Business.Services
+{
+    public class RoomTypeService : IService<RoomType>
+    {
+        private readonly RoomTypeRepository _roomTypeRepository;
+        public RoomTypeService(RoomTypeRepository rtRep)
+        {
+            _roomTypeRepository = rtRep;
+        }
+        public void Add(RoomType entity)
+        {
+            _roomTypeRepository.Add(entity);
+        }
+
+        public void Delete(Guid id)
+        {
+            _roomTypeRepository.Delete(id);
+        }
+
+        public IEnumerable<RoomType>? GetAll()
+        {
+            return _roomTypeRepository.GetAll();
+        }
+
+        public RoomType? GetById(Guid id)
+        {
+            return _roomTypeRepository.GetById(id);
+        }
+
+        public void Update(RoomType entity)
+        {
+            _roomTypeRepository.Update(entity);
+        }
+    }
+}
