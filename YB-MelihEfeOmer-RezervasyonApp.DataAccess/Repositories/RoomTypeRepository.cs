542b041f3ba312333003ac6da76fe0d4f62025bc
Ömer Tanır <tanir.omer98@gmail.com>
2024-07-22 14:50:49 +0300

    Data Access Layer'ına Abstractions dosyasına IRepository interface'i eklendi. Jenerik repository'ler Repositories dosyasına eklendi. Henüz dbcontext olmadığı için bazı kodlar yorum satırı olarak bırakıldı.

---
+++ YB-MelihEfeOmer-RezervasyonApp.DataAccess/Repositories/RoomTypeRepository.cs
@@ -0,0 +1,12 @@
+﻿using YB_MelihEfeOmer_RezervasyonApp.Entity.Models;
+
+namespace YB_MelihEfeOmer_RezervasyonApp.DataAccess.Repositories
+{
+    public class RoomTypeRepository : GenericRepository<RoomType>
+    {
+        //public RoomTypeRepository(ApplicationDbContext context) : base(context)
+        //{
+            
+        //}
+    }
+}
