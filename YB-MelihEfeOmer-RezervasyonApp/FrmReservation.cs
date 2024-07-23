9b8c384023e0ebd166f40b06d7e24d00d80731b4
Efe Can Demir <efecandemir7887@gmail.com>
2024-07-23 09:15:45 +0300

    Otellerin combobox'a getirilmesi.

--- YB-MelihEfeOmer-RezervasyonApp/FrmReservation.cs
+++ YB-MelihEfeOmer-RezervasyonApp/FrmReservation.cs
@@ -7,19 +7,40 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using YB_MelihEfeOmer_RezervasyonApp.Business.Services;
+using YB_MelihEfeOmer_RezervasyonApp.DataAccess.Context;
+using YB_MelihEfeOmer_RezervasyonApp.DataAccess.Repositories;
+using YB_MelihEfeOmer_RezervasyonApp.Entity.Models;
 
 namespace YB_MelihEfeOmer_RezervasyonApp
 {
     public partial class FrmReservation : Form
     {
+        HotelService hotelService;
+        HotelRepository hotelRepository;
+        BookingService bookingService;
+        BookingRepository bookingRepository;
         public FrmReservation()
         {
             InitializeComponent();
+            ApplicationDbContext context = new();
+            hotelRepository = new HotelRepository(context);
+            hotelService = new HotelService(hotelRepository);
+            bookingRepository = new BookingRepository(context);
+            bookingService = new BookingService(bookingRepository);
         }
 
         private void FrmReservation_Load(object sender, EventArgs e)
         {
+            ListHotels();
+        }
 
+        private void ListHotels()
+        {
+            cmbOtelAdi.DataSource = hotelService.GetAll();
+            cmbOtelAdi.DisplayMember = "Name";
+            cmbOtelAdi.ValueMember = "Id";
+            cmbOtelAdi.SelectedIndex = 0;
         }
 
         private void exitButton_Click(object sender, EventArgs e)
@@ -30,6 +51,11 @@ namespace YB_MelihEfeOmer_RezervasyonApp
         private void minimalizeButton_Click(object sender, EventArgs e)
         {
             this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void btnOdaBul_Click(object sender, EventArgs e)
+        {
+           
         }
     }
 }
