16680684e86d745c21ec9ff012f017a37110e649
Ömer Tanır <tanir.omer98@gmail.com>
2024-07-23 18:56:25 +0300

    Booking ve Guest tabloları arasında köprü olan BRBookingGuest tablosu için repository ve service eklendi.

---
+++ YB-MelihEfeOmer-RezervasyonApp.DataAccess/Repositories/BRBookingGuestRepository.cs
@@ -0,0 +1,13 @@
+﻿using YB_MelihEfeOmer_RezervasyonApp.DataAccess.Context;
+using YB_MelihEfeOmer_RezervasyonApp.Entity.Models;
+
+namespace YB_MelihEfeOmer_RezervasyonApp.DataAccess.Repositories
+{
+    public class BRBookingGuestRepository : GenericRepository<BRBookingGuest>
+    {
+        public BRBookingGuestRepository(ApplicationDbContext context) : base(context)
+        {
+            
+        }
+    }
+}
