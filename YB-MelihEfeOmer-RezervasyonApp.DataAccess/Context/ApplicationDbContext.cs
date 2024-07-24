033c2ae639a7614611e0bae8c135da8f4703af41
Mehmet Alakuş <mehmetalakus02@icloud.com>
2024-07-24 10:28:59 +0300

    Rezervasyon Arama Forma Eklendi.

--- YB-MelihEfeOmer-RezervasyonApp.DataAccess/Context/ApplicationDbContext.cs
+++ YB-MelihEfeOmer-RezervasyonApp.DataAccess/Context/ApplicationDbContext.cs
@@ -25,7 +25,9 @@ namespace YB_MelihEfeOmer_RezervasyonApp.DataAccess.Context
             //optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-DERU;Initial Catalog=HotelRezervationApp;Integrated Security=True;Trust Server Certificate=True"); // Ömer'in connection string'i
             //optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-93GN49G\SQLEXPRESS;Initial Catalog=HotelRezervationApp;Integrated Security=True;Trust Server Certificate=True"); //Efe'nin connection string'i
 
-            optionsBuilder.UseSqlServer(@"Data Source=BPC9\SQLEXPRESS;Initial Catalog=HotelRezervationApp;Integrated Security=True;Trust Server Certificate=True"); //Melih Bll Bilgisayar
+            //optionsBuilder.UseSqlServer(@"Data Source=BPC9\SQLEXPRESS;Initial Catalog=HotelRezervationApp;Integrated Security=True;Trust Server Certificate=True"); //Melih Bll Bilgisayar
+
+            optionsBuilder.UseSqlServer(@"Data Source = BPC18\SQLEXPRESS; Initial Catalog=HotelRezervationApp;Integrated Security=True;Trust Server Certificate=True"); //mehmet bll bilgisayar
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
